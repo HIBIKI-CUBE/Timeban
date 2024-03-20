3b69c2e078e9218f02013e54e96aa49578164301
HIBIKI <hibiki.apple.1984@gmail.com>
2024-03-21 01:04:51 +0900

    Fix: ログイン後に情報が更新されない不具合を解消

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
+  import { browser } from '$app/environment';
   import { enhance } from '$app/forms';
+  import { invalidateAll } from '$app/navigation';
   import type { PageData } from './$types';
 
   export let data: PageData;
 
   let { boards, session } = data;
   $: ({ boards, session } = data);
+  let reloadedAfterSignIn = false;
+
+  $: {
+    if (browser && session?.user.id && !reloadedAfterSignIn) {
+      invalidateAll();
+      reloadedAfterSignIn = true;
+    }
+  }
+
 </script>
 
 {#if session?.user.id}
@@ -19,7 +30,6 @@
   <form action="?/createBoard" method="post" use:enhance>
     <label for="name">ボードを作成</label>
     <input type="text" name="name" required />
-    <!-- <input type="hidden" name="owner" > -->
     <input type="submit" value="作成" />
   </form>
 {/if}
