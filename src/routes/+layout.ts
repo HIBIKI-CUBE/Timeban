4745f5e7bd049aa247dda61ebdbd8f39c27a9224
HIBIKI <hibiki.apple.1984@gmail.com>
2024-03-24 22:53:10 +0900

    Add: Vercel Utils

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,12 @@
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import type { LayoutLoad } from './$types';
 import { createBrowserClient, isBrowser, parse } from '@supabase/ssr';
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+
+injectSpeedInsights();
+inject({ mode: dev ? 'development' : 'production' });
 
 export const load: LayoutLoad = async ({ fetch, data, depends }) => {
   depends('supabase:auth');
