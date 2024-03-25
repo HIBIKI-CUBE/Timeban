0ce161558bdd408defd1aed0134f581ce82d634a
HIBIKI <hibiki.apple.1984@gmail.com>
2024-03-26 00:15:15 +0900

    Add: ローディングアニメーションを追加

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
   import { onMount } from 'svelte';
   import type { PageData } from '../routes/$types';
   import { invalidate } from '$app/navigation';
+  import { navigating } from '$app/stores';
 
   export let data: PageData;
 
@@ -24,10 +25,25 @@
 </script>
 
 <AuthController {data} />
+{#if $navigating}
+  <img class="loading" src="logoAnim.svg" alt="" />
+{/if}
 <slot />
 
-<style>
+<style lang="scss">
   :global(body) {
     margin: 0;
+  }
+  .loading {
+    position: fixed;
+    bottom: 1em;
+    right: 1em;
+    width: 3em;
+    height: 3em;
+    background-color: #2b2b2b;
+    padding: 0.5em;
+    box-sizing: border-box;
+    border-radius: 0.5em;
+    z-index: 9999;
   }
 </style>
